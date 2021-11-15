78f5c05b92ffb452093283f528d87990a42b0c6e
Aso2001003 <83050625+Aso2001003@users.noreply.github.com>
2021-11-15 14:05:49 +0900

    Add files via upload

---
+++ PHPsourse/カート内ページ/cart.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>カート内ページ</title>
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/cart.css">
+</head>
+<body>
+<?php require 'header.php'; ?>
+<?php require 'footer.php'; ?>
