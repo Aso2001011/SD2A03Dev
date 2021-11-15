cafe9cd4e94909090dae7a77885b97e4aaa306d1
Aso2001003 <83050625+Aso2001003@users.noreply.github.com>
2021-11-15 14:07:39 +0900

    Add files via upload

---
+++ PHPsourse/お気に入りページ/fav.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>お気に入りページ</title>
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/fav.css">
+</head>
+<body>
+<?php require 'header.php'; ?>
+<?php require 'footer.php'; ?>
