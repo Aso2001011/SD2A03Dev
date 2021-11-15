313f08da1483090d557de96db70a1237f23cb47a
Aso2001003 <83050625+Aso2001003@users.noreply.github.com>
2021-11-15 14:03:39 +0900

    Add files via upload

---
+++ PHPsourse/ログインページ/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ログインページ</title>
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
+</head>
+<body>
+<?php require 'header.php'; ?>
+<?php require 'footer.php'; ?>
