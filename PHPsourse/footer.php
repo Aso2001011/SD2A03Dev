2c6ef608442440348d55774174676bd2239d2552
Aso2001003 <83050625+Aso2001003@users.noreply.github.com>
2021-11-15 13:47:22 +0900

    Add files via upload

---
+++ PHPsourse/footer.php
@@ -0,0 +1,44 @@
+</div>
+<!-------------------------------------------->
+<footer>
+    <div class="foot-wrap">
+        <div class="menu-left">
+            <h3>メニュー</h3>
+            <ul class="foot-left">
+                <li style="display: none">f</li>
+                <li><a href="#">ログイン</a></li>
+                <li><a href="#">会員情報</a></li>
+                <li><a href="#">お気に入り</a></li>
+                <li><a href="#">カート</a></li>
+            </ul>
+        </div>
+        <div class="menu-center">
+            <h3>カテゴリ</h3>
+            <ul class="foot-center">
+                <li style="display: none">f</li>
+                <li><a href="#">デスクトップPC</a></li>
+                <li><a href="#">ノートPC</a></li>
+                <li><a href="#">ゲーミングPC</a></li>
+                <li><a href="#">クリエイター向けPC</a></li>
+                <li><a href="#">周辺機器</a></li>
+            </ul>
+        </div>
+        <div class="menu-right">
+            <h3>サポート</h3>
+            <ul class="foot-right">
+                <li style="display: none">f</li>
+                <li><a href="#">新規会員登録</a></li>
+                <li><a href="#">ご利用ガイド</a></li>
+                <li><a href="#">お問い合わせ</a></li>
+                <li><a href="#">サポート</a></li>
+            </ul>
+        </div>
+        <small class="cmark">©2021システム開発3班</small>
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<!--自作のJS-->
+<script src="./script/script.js"></script>
+</body>
+</html>
---
+++ PHPsourse/header.php
@@ -0,0 +1,31 @@
+<header>
+    <h1><a href="index.php">T　I　T　L　E</a></h1>
+    <nav class="menu">
+        <ul class="nav_menu1">
+            <li class="nav_menu1_item"><a href="" class="">新規会員登録</a></li>
+            <li class="nav_menu1_item"><a href="" class="">ご利用ガイド</a></li>
+            <li class="nav_menu1_item"><a href="" class="">お問い合わせ</a></li>
+            <li class="nav_menu1_item"><a href="" class="">サポート</a></li>
+        </ul>
+        <ul class="nav_menu2">
+            <li class="nav_menu2_item login"><a href="login.php" class=""><i class="fas fa-sign-in-alt"></i> ログイン</a></li>
+            <li class="nav_menu2_item"><a href="info.php" class=""><i class="fas fa-user-circle"></i> 会員情報</a></li>
+            <li class="nav_menu2_item"><a href="fav.php" class=""><i class="fas fa-heart"></i> お気に入り</a></li>
+            <li class="nav_menu2_item"><a href="cart.php" class=""><i class="fas fa-shopping-cart"></i> カート</a></li>
+            <li class="nav_menu2_item glass">
+                <form action="search.php" method="post">
+                    <input type="text" name="search" class="search" placeholder="何をお探しですか？">
+                    <button type="submit"><i class="fas fa-search"></i></button><span>検索</span>
+                </form>
+            </li>
+        </ul>
+        <ul class="nav_menu3">
+            <li class="nav_menu3_item"><a href="category.php" class="">デスクトップPC</a></li>
+            <li class="nav_menu3_item"><a href="category.php" class="">ノートPC</a></li>
+            <li class="nav_menu3_item"><a href="category.php" class="">ゲーミングPC</a></li>
+            <li class="nav_menu3_item"><a href="category.php" class="">クリエイター向けPC</a></li>
+            <li class="nav_menu3_item"><a href="category.php" class="">周辺機器</a></li>
+        </ul>
+    </nav>
+</header>
+<div class="cont">
