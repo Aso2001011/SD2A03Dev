0a2bba91f1fa463b2d5d771411c55f169454e1cf
[Aso2001018] <[2001018@s.asojuku.ac.jp]>
2021-12-15 14:59:16 +0900

    upload files

--- PHPsourse/connect-php/login-connect.php
+++ PHPsourse/connect-php/login-connect.php
@@ -3,24 +3,18 @@ session_start();
 require 'getDBSql.php';
 if (isset($_SESSION['login'])) {
   if (isset($_POST['mail'])&&isset($_POST['pass'])) {
-    $ret = getDbSql('SELECT * FROM m_customers WHERE mail = "'.$_POST['mail'].'" AND password = "'.$_POST['pass'].'";');
+    $ret = getDbSql('SELECT * FROM m_user WHERE mail = "'.$_POST['mail'].'" AND password = "'.$_POST['pass'].'";');
     if(count($ret) == 1) {
       $_SESSION['login'] = true;
       $_SESSION['user_id'] = $ret[0]['id'];
-      $_SESSION['name'] = $ret[0]['name'];
-      $_SESSION['tel'] = $ret[0]['tel'];
-      $_SESSION['pass'] = $ret[0]['password'];
-      $_SESSION['mail'] = $ret[0]['mail'];
       http_response_code(301);
       header("Location: ../index.php");
-      endDbSql();
       exit;
     }
     else {
       $_SESSION['msg'] = '一致するアカウントが見つかりませんでした。';
       http_response_code(301);
       header("Location: ../login.php");
-      endDbSql();
       exit;
     }
   }
@@ -31,14 +25,12 @@ if (isset($_SESSION['login'])) {
       $_SESSION['msg'] = '一致するアカウントが見つかりませんでした。';
     http_response_code(301);
     header("Location: ../login.php");
-    endDbSql();
     exit;
   }
 }
 else {
 	http_response_code(301);
 	header("Location: ../login.php");
-  endDbSql();
 	exit;
 }
 ?>
